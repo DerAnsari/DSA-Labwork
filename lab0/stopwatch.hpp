54c10e04331382890067b4794a860da77b6b14f5
DerAnsari <ansarisarmad426@gmail.com>
2026-09-03 20:22:05 +0500

    i forgot to add some of the finishing touches lmfao

--- lab0/stopwatch.hpp
+++ lab0/stopwatch.hpp
@@ -4,18 +4,16 @@
 
 class Stopwatch {
 public:
-    Stopwatch() : start {std::chrono::high_resolution_clock::now()} {
-    }
-    void reset() {
-        start = std::chrono::high_resolution_clock::now();
-    }
-    // elapsed time in seconds
-    double elapsedTime() const {
-        const auto now = std::chrono::high_resolution_clock::now();
-        const auto duration = std::chrono::duration_cast<std::chrono::seconds>(now - start);
-        return duration.count();
-    }
-private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start; 
-};
+  Stopwatch() : start{std::chrono::high_resolution_clock::now()} {}
+  void reset() { start = std::chrono::high_resolution_clock::now(); }
+  // elapsed time in seconds
+  double elapsedTime() const {
+    const auto now = std::chrono::high_resolution_clock::now();
+    const auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(now - start);
+    return duration.count();
+  }
 
+private:
+  std::chrono::time_point<std::chrono::high_resolution_clock> start;
+};
